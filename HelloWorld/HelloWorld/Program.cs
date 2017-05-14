460de44e52e3d37ce87dc69a62b00fe522823163
Marek Lenart <marek.lenart@pl.abb.com>
2017-05-14 21:25:57 +0200

    zadanie 1

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj Swiecie!");
+            Console.WriteLine("Nacisnij dowolny przycisk by zakonczyc dzialanie aplikacji...");
+            Console.ReadKey();
+        }
+    }
+}
