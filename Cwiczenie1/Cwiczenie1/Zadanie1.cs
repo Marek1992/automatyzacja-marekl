0655d3e8d880ca9c940dbd3c017cc311dd44f363
Marek Lenart <marek.lenart@pl.abb.com>
2017-05-23 13:08:42 +0200

    cwiczenie1

---
+++ Cwiczenie1/Cwiczenie1/Zadanie1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie1
+{
+    public class Zadanie1
+    {
+        int liczbaPierwsza;
+        int liczbaDruga;
+
+        public Zadanie1(int a, int b)
+        {
+            liczbaPierwsza = a;
+            liczbaDruga = b;
+        }
+        public int Add()
+        {
+            return liczbaPierwsza + liczbaDruga;
+        }
+
+        public int Substract()
+        {
+            return liczbaPierwsza - liczbaDruga;
+        }
+
+        public int Multiply()
+        {
+            return liczbaPierwsza * liczbaDruga;
+        }
+
+        public double Divide()
+       {
+            return liczbaPierwsza / liczbaDruga;
+       }
+   }
+}
+
