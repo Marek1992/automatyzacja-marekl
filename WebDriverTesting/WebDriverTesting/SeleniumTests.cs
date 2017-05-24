6a077a4c4725c526f78a450eea485f7ff0653a71
Marek Lenart <marek.lenart@pl.abb.com>
2017-05-24 09:57:15 +0200

    selenium

---
+++ WebDriverTesting/WebDriverTesting/SeleniumTests.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+
+
+namespace WebDriverTesting
+{
+    public class TestCases : IDisposable
+    {
+        public TestCases()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage()
+                .Timeouts()
+                .ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+        [Fact]
+        public void FixMe()
+        {
+            Assert.Equal(true, false);
+        }
+        public void Dispose()
+        {
+            _driver.Quit();
+        }
+        private ChromeDriver _driver;
+    }
+}
