8190bd16dc30f12c84f969ad46dedcbb5d781f88
Marek Lenart <marek.lenart@pl.abb.com>
2017-05-24 14:22:11 +0200

    nowytest

--- WebDriverTesting/WebDriverTesting/SeleniumTests.cs
+++ WebDriverTesting/WebDriverTesting/SeleniumTests.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -32,6 +33,61 @@ namespace WebDriverTesting
             Assert.Equal("Vivamus aliquam feugiat", firstNoteTitle);
 
             
+        }
+        [Fact]
+        public void Zalogowanie_Dodatnie_Notki_I_Sprawdzenie_Czy_Notka_Wyswietla_Sie_Prawidlowo()
+        {
+            _driver.Navigate().GoToUrl("https://autotestdotnet.wordpress.com/wp-admin/");
+            var login = _driver.FindElementById("user_login");
+            login.Clear();
+            Thread.Sleep(1000);
+            login.SendKeys("autotestdotnet@gmail.com");
+
+            var password = _driver.FindElementById("user_pass");
+            password.Clear();
+            Thread.Sleep(1000);
+            password.SendKeys("codesprinters2016");
+
+            _driver.FindElementById("wp-submit").Click();
+            Thread.Sleep(1000);
+
+            _driver.FindElementById("menu-posts").Click();
+            Thread.Sleep(1000);
+
+            var newPost = _driver.FindElementByClassName("page-title-action");
+            Thread.Sleep(1000);
+            newPost.Click();
+
+            Thread.Sleep(1000);
+
+            var newUniqueName =  Guid.NewGuid().ToString();
+            var titlePost = _driver.FindElementByName("post_title");
+            titlePost.SendKeys(newUniqueName);
+            Thread.Sleep(1000);
+
+            var postText = _driver.FindElementByName("content");
+            postText.SendKeys("NowyPostMarek");
+            Thread.Sleep(1000);
+
+            var publish = _driver.FindElementByName("publish");
+            publish.Click();
+            Thread.Sleep(1000);
+
+            var avatar = _driver.FindElementByCssSelector(".avatar.avatar-32");
+            avatar.Click();
+            Thread.Sleep(1000);
+
+            var signOut = _driver.FindElementByClassName("ab-sign-out");
+            Thread.Sleep(1000);
+            signOut.Click();
+            Thread.Sleep(1000);
+
+            _driver.Navigate().GoToUrl("https://autotestdotnet.wordpress.com/");
+
+            var title = _driver.FindElementByXPath("//a[text()='"+ newUniqueName +"']").Text;
+            Thread.Sleep(1000);
+            Assert.Equal(newUniqueName, title);
+
         }
         public void Dispose()
         {
